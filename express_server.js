7b7aab89a19104894cf148c15f6721d365818181
C-Shi <cshi@ualberta.ca>
2018-09-04 15:34:48 -0600

    add new url route

--- express_server.js
+++ express_server.js
@@ -20,3 +20,7 @@ app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new.ejs');
+})
