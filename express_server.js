df9edcb189ee59a4c1b1a027a90d8e0cbd9e4044
C-Shi <cshi@ualberta.ca>
2018-09-04 15:32:12 -0600

    config express body-parser ejs view engine

---
+++ express_server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+const bodyParser = require('body-parser');
+var PORT = 8080; // default port 8080
+
+//config environment
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
