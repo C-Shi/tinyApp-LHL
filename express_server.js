7bdf2ecf086f8e03c3e159c227370d57e08c0d6a
C-Shi <cshi@ualberta.ca>
2018-09-04 15:32:54 -0600

    add roots /urls route

--- express_server.js
+++ express_server.js
@@ -11,3 +11,12 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/", (req, res) => {
+  res.send('HOME');
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
